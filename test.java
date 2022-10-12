8acd1bd93a96738c2d7036971d6200224ae8db7a
Lanpasto <77079137+Lanpasto@users.noreply.github.com>
2022-10-12 20:12:32 +0300

    Start1

--- test.java
+++ test.java
@@ -2,40 +2,15 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class test extends JFrame {
+public class test {
     private JButton button1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton clearButton;
-    private JLabel lblWelcome;
-    private JPanel mainPannel;
 
     public test() {
-       setContentPane(mainPannel);
-       setTitle("Welcome");
-       setSize(450,300);
-       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-       setVisible(true);
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String firstName = textField1.getText();
-                String lastName = textField2.getText();
-                lblWelcome.setText("Welcome " + firstName + " " + lastName);
+
             }
         });
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField1.setText("");
-                textField2.setText("");
-                lblWelcome.setText("");
-            }
-        });
-    }
-
-    public static void main(String[] args) {
-        test MyFrame = new test();
-
     }
 }
